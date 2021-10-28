2d1a60cce3bf5d0108bb8b26bf778257c52f38d5
awibb <andrew_wibbenmeyer@outlook.com>
2021-10-27 22:18:21 -0500

    Changed to capture to every 10 frames. Added error handling for if theres an issue writing a picture to the local system. commented out the drawing of the bouding box.

--- ssd_mobilenet_v3/main.py
+++ ssd_mobilenet_v3/main.py
@@ -1,4 +1,5 @@
-import cv2  # pip install opencv-python
+import cv2 # pip install opencv-python
+
 config_file = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 frozen_model = 'frozen_inference_graph.pb'
 model = cv2.dnn_DetectionModel(frozen_model, config_file)
@@ -13,19 +14,18 @@ with open(file_name, 'rt') as fpt:
     classLabels = fpt.read().rstrip("\n").split("\n")
 print(classLabels)
 
-
-model.setInputSize(320, 320)  # image resolution
-model.setInputScale(1.0/127.5)
-model.setInputMean((127.5, 127.5, 127.5))  # mobilenet => [-1,1]
+# This is probably able to be
+model.setInputSize(320, 320) # image resolution
+model.setInputScale(1.0 / 127.5)
+model.setInputMean((127.5, 127.5, 127.5)) # mobilenet => [-1,1]
 model.setInputSwapRB(True)
 
-
-cap = cv2.VideoCapture("Videos/test_2.mp4")
+cap = cv2.VideoCapture("Videos/team_demo.mp4")
 font_scale = 2
 font = cv2.FONT_HERSHEY_PLAIN
 
 padding = 5
-frameCount = 6
+frameCount = 0
 
 if cap.isOpened():
     while True:
@@ -34,24 +34,27 @@ if cap.isOpened():
         ClassIndex, confidence, bbox = model.detect(frame, confThreshold=0.6)
 
         if len(ClassIndex) != 0:
-            for ClassInd, conf, boxes in zip(ClassIndex.flatten(), confidence.flatten(), bbox):
+            for ClassInd, conf, boxes in zip(
+                ClassIndex.flatten(), confidence.flatten(), bbox
+            ):
 
                 # crop the photo and save it in the output folder
                 x, y, x1, y1 = boxes
-
-                if (frameCount % 7 == 0):
-
-                    # frame[height:height+height, width:width+width] with additonal padding
-                    crop_img = frame[y-padding:y1+y +
-                                     padding, x-padding:x1+x+padding]
-                    id += 1
-                    image_name = outputFolder + str(id) + ".jpg"
-                    cv2.imwrite(image_name, crop_img)
-                    print(image_name)
-
                 # draw the bounding box
                 if ClassInd == 1:
-                    cv2.rectangle(frame, boxes, (255, 0, 0), 2)
+                    #clscommented out for now since we don't want it drawn but not removing in case we need it for latercv2.rectangle(frame, boxes, (255, 0, 0), 2)
+                    if (frameCount % 10 == 0):
+                        # frame[height:height+height, width:width+width] with additonal padding
+                        crop_img = frame[y - padding:y1 + y + padding,
+                                         x - padding:x1 + x + padding]
+
+                        image_name = outputFolder + str(id) + ".jpg"
+                        try:
+                            cv2.imwrite(image_name, crop_img)
+                            id += 1
+                            print(image_name)
+                        except:
+                            continue
 
         # Display the resulting frame
         cv2.imshow('Person Detection', frame)
