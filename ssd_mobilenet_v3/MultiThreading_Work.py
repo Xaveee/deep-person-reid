2ae929eab522841c795fbb5f885bc3396045b290
awibb <andrew_wibbenmeyer@outlook.com>
2021-11-18 18:24:53 -0600

    fixed some missing imports and broken declerations

--- ssd_mobilenet_v3/MultiThreading_Work.py
+++ ssd_mobilenet_v3/MultiThreading_Work.py
@@ -7,19 +7,21 @@ from torchreid.utils.feature_extractor import FeatureExtractor
 import threading
 import os
 import glob
+from queue import Queue
+from multiprocessing import Process
 
 config_file = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 frozen_model = 'frozen_inference_graph.pb'
 feature_model = 'osnet_ibn_x1_0_duke_256x128_amsgrad_ep150_stp60_lr0.0015_b64_fb10_softmax_labelsmooth_flip.pth'
 gallery_feature_df = pd.read_csv('data\gallery_data.csv')
 model = cv2.dnn_DetectionModel(frozen_model, config_file)
-extractor = FeatureExtractor(model_name='osnet_x1_0', model_path=feature_model, device='cpu')
+extractor = FeatureExtractor(
+    model_name='osnet_x1_0', model_path=feature_model, device='cpu'
+)
 
 # output folder and the id start number change these!!!!!!
 outputFolder = "data\gallery\gal"
 queryFolder = 'data\query\query'
-
-
 """
 cam_worker(query_queue, feature_extractor, cam):
     cap = cv2.VideoCapture(cam)
@@ -29,7 +31,10 @@ cam_worker(query_queue, feature_extractor, cam):
         query_queue.put(feature)
     cap.close()
 """
+
+
 class cam_worker(threading.Thread):
+
     def __init__(self, query_queue, feature_extractor, cam):
         threading.Thread.__init__(self)
         self.query_queue = query_queue
@@ -38,12 +43,12 @@ class cam_worker(threading.Thread):
 
     def run(self):
         cap = cv2.VideoCapture(self.cam)
-        while(cap.isOpened()):
+        while (cap.isOpened()):
             # Extract features
             # Append features to query queue
             query_queue.put(feature)
         cap.close()
-     
+
 
 """
 comparing_worker(query_queue, gallery):
@@ -56,19 +61,24 @@ comparing_worker(query_queue, gallery):
         # Perform Comparison. Maybe every 5 feature or 5 second? Comparing every n
         # The item(features) from the query has a camID column. ONLY compare with other item that has DIFFERENT camID
 """
+
+
 class comparing_worker(threading.Thread):
+
     def __init__(self, query_queue, gallery):
         threading.Thread.__init__(self)
         self.query_queue = query_queue
         self.gallery = gallery
-    while(1):
+
+    while (1):
         if query_queue.is_empty():
             continue
-            
+
         # Get item from query queue
         query_queue.get()
         # Perform Comparison. Maybe every 5 feature or 5 second? Comparing every n
         # The item(features) from the query has a camID column. ONLY compare with other item that has DIFFERENT camID
+
 
 """
 main(cam_list, feature_extractor):
@@ -81,13 +91,16 @@ main(cam_list, feature_extractor):
     
     process = Process(target=comparing_worker, arg=(query_queue, gallery)
 """
-main(cam_list, feature_extractor):
+
+
+def main(cam_list, feature_extractor):
     # initialize query_queue and gallery
     query_queue = Queue()
     gallery = []
     for cam in cam_list:
-        process = Process(target=cam_worker, arg=(query_queue, feature_extractor, cam)
-        process.start
-        
-    
-    process = Process(target=comparing_worker, arg=(query_queue, gallery)
+        process = Process(
+            target=cam_worker, arg=(query_queue, feature_extractor, cam)
+        )
+        process.start()
+
+    process = Process(target=comparing_worker, arg=(query_queue, gallery))
