4510f1e66c3581513f736ef17784cdb30b232aa6
Krawczyk <ppk1500@live.missouristate.edu>
2021-12-10 19:57:22 -0600

    added comments

--- ssd_mobilenet_v3/re-id.py
+++ ssd_mobilenet_v3/re-id.py
@@ -1,4 +1,3 @@
-#from sys import last_traceback
 import cv2
 import numpy as np
 import pandas as pd
@@ -9,58 +8,96 @@ import os
 from multiprocessing import Process, Queue
 from utilities import birch_clustering, DBSCAN_clustering, mean_shift_clustering, get_output, visualization
 
-# output folder and the id start number change these!!!!!!
-outputFolder = "data\gallery\gal"
-queryFolder = 'data\query\query'
 
+# function by which each camera thread runs
+# Functional requirement 10 in SRS
 
 def cam_worker(query_queue, feature_extractor, cam, cam_id):
+    # Initializing arguments for human detection extarctor
+
     config_file = 'Models/detection/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
     frozen_model = 'Models/detection/frozen_inference_graph.pb'
     detection_model = cv2.dnn_DetectionModel(frozen_model, config_file)
-    detection_model.setInputSize(320, 320) # image resolution
+    detection_model.setInputSize(320, 320) 
     detection_model.setInputScale(1.0 / 127.5)
-    detection_model.setInputMean((127.5, 127.5, 127.5)) # mobilenet => [-1,1]
+    detection_model.setInputMean((127.5, 127.5, 127.5)) 
     detection_model.setInputSwapRB(True)
 
     padding = 5
     frame_count = 0
 
     print('starting cam', cam_id)
+
+    # next line can be uncommented if user wants to run program on already recorded videos
     # cap = cv2.VideoCapture(os.path.join('Videos/thread', cam))
     cap = cv2.VideoCapture(int(cam_id)+1)
     while cap.isOpened():
+
+        # Increasing frame count with each iteration
+
         frame_count += 1
+
+        # Gathering frame from camera input
+
         ret, frame = cap.read()
+
+        # If camera is not active, printing message to a user
+        # Hardware requirement 2 in SRS
+
         if (ret == False):
             print('cam', cam_id, 'ended')
             break
+
+        # Resizing an gathered frame
+
         frame = cv2.resize(frame, (320, 320))
+
+        # Executing main body of human detection and feature extraction in 5 frame intervals
+        # Non-funcional requirement 1 in SRS
+
         if (frame_count % 5 == 0):
+
+            # Human detection module with threshold set to 0.6
+            # Start of human detection module described in SDD
+
             ClassIndex, confidence, bbox = detection_model.detect(
                 frame, confThreshold=0.6
             )
+
+            # If human figures got detected it starts cropping images of detected people
+            # Non-functional requirement 5 in SRS
+
             if len(ClassIndex) != 0:
+
+                # Looping through each detected person to perform feature extraction
+
                 for ClassInd, conf, boxes in zip(
                     ClassIndex.flatten(), confidence.flatten(), bbox
                 ):
                     x, y, w, h = boxes
+
+                    # Extraction of crop image of a detected person
+                    # And adding camera id and frame count to a image name
+
                     if ClassInd == 1:
                         crop_img = frame[y:y + h, x:x + w]
                         img_name = cam_id + '_' + str(frame_count) + '.jpg'
                         gal_folder = 'data/gallery/{}'.format(cam_id)
                         cv2.imwrite(
                             os.path.join(gal_folder, img_name),
-                            # cv2.resize(crop_img, (360, 640))
-                            # cv2.resize(crop_img, (640, 480))
                             crop_img
                         )
                         print('Extracting', img_name)
+
+                        # Extraction of features from the image
+                        # Functional requirement 2 in SRS
+
                         img_feature = feature_extractor(
                             os.path.join(gal_folder, img_name)
                         )
-                        # query_queue.put(img_feature)
-                        # print(img_feature)
+
+                        # Gathering time stamps when image got taken and on which camera
+                        # Functional requirement 8 in SRS
 
                         img_feature_df = pd.DataFrame(img_feature.numpy())
                         img_feature_df.insert(0, 'actual_time', (datetime.now()-datetime(1970, 1, 1)).total_seconds())
@@ -68,82 +105,120 @@ def cam_worker(query_queue, feature_extractor, cam, cam_id):
                         img_feature_df.insert(0, 'cam_id', cam_id)
                         img_feature_df.insert(0, 'filename', img_name)
                         img_feature_df.insert(0, 'frame', frame_count)
-                        # print(img_feature_df)
+
+                        # Adding feature list of a person to a queue
+
                         query_queue.put(img_feature_df)
+
+
+        # Displaying camera input
 
         cv2.imshow('Camera ' + cam_id, cv2.resize(frame, (640, 480)))
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print('killing cam', cam_id)
             query_queue.close()
             break
-        # Extract features
-        # Append features to query queue
-        # query_queue.put(feature_extractor)
+    
     cap.release()
     cv2.destroyAllWindows()
 
 
-
+# function on which comparing server runs
+# it takes as a parameter feature queue and gallery defined more specificaly in SDD section 2.2 
+# function performs reidentification and updates visualization folder and output csv files
 
 def comparing_worker(query_queue, gallery):
-    '''
-    Comparing component
-    This component get the features of a new image from the query queue, then cluster and label them.
-    Output csv's columns: frame count - file name - cam id - person id - 5 people in the same cluster (not closest)
-    '''
-    # counter = 0  # unused
+
+    # Gathering time when comparizon module starts
+
     latest_save_time = datetime.now()
+
+    # Starting main loop of reidentification module described in detailed in SDD section 3.2.3
+
     while (1):
+
+        # checking if query queue is empty
+
         if query_queue.empty():
-            # If the queue is empty, keep looping
             continue
-        # Get item from query queue
+
+        # Getting item from a query queue and grabing values for each feature and putting them in a list
+
         feature = query_queue.get()
         np_feature = feature.values
-        # print('working...')
+        
+        # Initializing gallery with list of features if gallery is empty
+        # Otherwise adding list of features into gallery
+
         if gallery == [[]]:
-            # If gallery is empty, set the first sample as gallery
             gallery = np_feature
         else:
-            # If the gallery has data, add the new sample to gallery
             gallery = np.concatenate((gallery, np_feature), axis=0)
+
+        # Performing clastering each 100 frames input 
+
         if gallery.shape[0] % 100 != 0:
-            # Perform clustering every 100 frames input
             continue
-        # START COMPARISON.
+
+        # Starting Reidentification main module 
+        # For each element in gallery grabing all features and putting it into 2D list 
+
         feature_arr = gallery[:, 4:]
-        # We can change between DBSCAN_clustering, birch_clustering and mean_shift_clustering
+
+        # Clustering and comparing of collected features and merging them with ids, new cluster is created for each new person
+        # Functional requirement 3, 4 and 5 in SRS
+        # Non-functional requirement 6
+
         label = DBSCAN_clustering(feature_arr, eps = 4.6).reshape(-1, 1)
         labeled_arr = np.concatenate((gallery, label), axis=1)
 
-        # print(labeled_arr)
-        # END COMPARISON
-        # counter += 1
+        
+        # End of reidentification process
+        # Start of saving clustered data into visualization folder and csv files
+
+        # Gathering current time
+        
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
+
+        # Saving process is executed each 10 seconds
+
         if (now-latest_save_time).total_seconds() >= 10:
-            # Update features csv and labeled csv every 30s
+            # Converting clustered list into formated data list with closest neighbors
+            # Desribed in detailed in SDD section 3.2.3.4
+
             out_df = pd.DataFrame(get_output(labeled_arr))
+
+            # Saving clustered data into csv files in local database with labeling
+            # Logical database requirement 3 and 4 in SRS
+
             out_df.columns = ['Frame Count', 'File Name', 'Camera ID', 'Time Stamp', 'Person ID', '1st Match', '2nd Match', '3rd Match', '4th Match', '5th Match']
             out_df.to_csv('data/labeled_gal.csv', index=False)
             visualization(out_df)
             gal_df = pd.DataFrame(gallery)
             gal_df.to_csv('data/gallery.csv', header=False, index=False)
-            # reset latest save time
             print()
             print('SAVED AT', current_time, end='\n\n')
+
+            # updating time of last time saving process ended
+
             latest_save_time = now
         else:
             pass
 
-        # The item(features) from the query has a camID column. ONLY compare with other item that has DIFFERENT camID
 
 
 
 def main(cam_list, feature_extractor):
     # initialize query_queue and gallery
+    # described in detailed in SDD section 2.2
+
     query_queue = Queue()
     gallery = [[]]
+
+    # for each camera starting individual thread
+    # functional requirement 10 in SRS
+
     for cam_id, cam in enumerate(cam_list):
         try:
             os.mkdir('data/gallery/' + str(cam_id))
@@ -155,18 +230,38 @@ def main(cam_list, feature_extractor):
         )
         process.start()
 
+    # starting reidentification module
+    # described in detailed in SDD section 3.2.3
+
     process = Process(target=comparing_worker, args=(query_queue, gallery))
     process.start()
 
 
 if __name__ == '__main__':
+    # creating list for cameras
     cam_list = os.listdir('Videos/thread')
+
+    # initializing model for feature extractor
     feature_model = 'Models/osnet/osnet_ms_d_c.pth.tar'
 
     feature_extractor = FeatureExtractor(
         model_name='osnet_x1_0', model_path=feature_model, device='cpu'
     )
-    # print(cam_list)
+
+    # Number of cameras, can be changed by the user
     cam_num = 2
     cam_list = [''] * cam_num
     main(cam_list, feature_extractor)
+
+# Copyright 2021 Missori State University
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
--- ssd_mobilenet_v3/utilities.py
+++ ssd_mobilenet_v3/utilities.py
@@ -3,105 +3,71 @@ import numpy as np
 from numpy.core.numeric import identity
 from sklearn.preprocessing import StandardScaler
 import umap
-from sklearn.cluster import Birch
 from sklearn.cluster import DBSCAN
-from sklearn.cluster import MeanShift
 from umap.umap_ import nearest_neighbors
 import os
 import shutil
 
 
-def birch_clustering(feature, threshold=5.3):
-    '''
-    Function to perform Birch clustering
-    Performance may be poor when the number of features and/or samples increases
-    Predetermined threshold = 5.3
-    feature: The numpy array contain all features of samples to be clustered
-    Output: The label of input array
-    '''
-    reducer = umap.UMAP(random_state=1, metric='manhattan')
-    scaled_feature = StandardScaler().fit_transform(feature)
-    embedding = reducer.fit_transform(feature)
 
-    clustering = Birch(
-        threshold=threshold,
-        branching_factor=50,
-        n_clusters=None,
-        compute_labels=True
-    ).fit(embedding)
-
-    return clustering.labels_
-
+# function performing clustering over features gallery
+# it takes as a parameters feature list and predifined treshold, in this case setted to 3.1
+# output is clustered labeled array
+# defined in SDD section 3.2.3
 
 def DBSCAN_clustering(feature, eps=3.1):
-    '''
-    Function to perform DBSCAN clustering
-    Predetermined epsilon = 3.1
-    feature: The numpy array contain all features of samples to be clustered
-    Output: The label of input array
-    '''
+    # initializing parameters for clustering algorithm
     reducer = umap.UMAP(random_state=1, metric='manhattan')
-    #scaled_feature = StandardScaler().fit_transform(feature)
     embedding = reducer.fit_transform(feature)
 
+    # running DBSCAN clustering algorithm
     clustering = DBSCAN(eps=eps).fit(embedding)
 
     return clustering.labels_
 
 
-def mean_shift_clustering(feature, threshold=4.6):
-    '''
-    Function to perform Mean Shift clustering
-    Predetermined threshold = 4.6
-    feature: The numpy array contain all features of samples to be clustered
-    Output: The label of input array
-    '''
-    reducer = umap.UMAP(random_state=1, metric='manhattan')
-    scaled_feature = StandardScaler().fit_transform(feature)
-    embedding = reducer.fit_transform(feature)
-
-    clustering = MeanShift(bandwidth=threshold).fit(embedding)
-
-    return clustering.labels_
-
+# function that grabs list of features and finds 5 closest neighbors
+# it takes as parameters chosen list of features and clusterd and labeled list of features
+# it outputs list with 5 closest neighbors of input chosen list of features
+# defined in SDD section 3.2.3
 
 def get_nearest_neighbor(chosen_arr, labeled_arr):
-    '''
-    Function to get the 5 neighbors with the oldest time recorded of a chosen array
-    chosen_arr: The array to be compared
-    labeled_arr: The labled dataset
-    Output: An array contains 5 neighbors of the chosen point 
-    '''
+    # grabing all list of features in the same cluster as chosen list of features
+
     same_clust = labeled_arr[labeled_arr[:, -1] == chosen_arr[-1], :]
     same_clust = same_clust[same_clust[:, 2] != chosen_arr[2], :]
 
+    # if there is no matches filling list with message to a user
     if same_clust.size <= 5:
-        # In case there is no match
         return np.array(['No similar matches', '', '', '', ''])
+
+    # sorting list of matches and grabbing first 5 elements
     same_clust = same_clust[np.argsort(same_clust[:, 4])]
     same_clust = same_clust[:5]
-    #print(same_clust[:, 0])
+    
+    # putting each element into output list
     nearest_neighbors = []
     for sample in same_clust:
         nearest_neighbors = np.append(nearest_neighbors, 'cam ' + str(sample[2]) + ' frame count ' + str(sample[0]))
-    # print(nearest_neighbors)
+    
     return nearest_neighbors
 
+# function that prepars labeled array to be saved as csv file
+# it grabs labaled dataset as input
+# it outputs list ready to be converted to csv file
+# defined in SDD section 3.2.3
 
 def get_output(labeled_arr):
-    '''
-    Function to get output array to be saved to csv
-    labeled_arr: The labeled dataset
-    Output: A numpy array with all the samples and their framecount - file name - cam id - label (person id) - 5 matches if possible, 
-    '''
+    
     misc_arr = labeled_arr[:, [0, 1, 2, 3, -1]]
 
     additional_arr = [[]]
+    # looping through each person in labaled dataset
     for sample in labeled_arr:
+        # getting nearest neighbors for current person
         nearest_neighbors = get_nearest_neighbor(sample, labeled_arr)
-        # print(nearest_neighbors)
-        # print()
-        #print(nearest_neighbors, 5 - nearest_neighbors.shape[0])
+        
+        # formating data in a list
         if nearest_neighbors.shape[0] < 5:
             nearest_neighbors = np.pad(nearest_neighbors, (0, 5 - nearest_neighbors.shape[0]), 'constant', constant_values=('', ''))
         if additional_arr == [[]]:
@@ -110,25 +76,48 @@ def get_output(labeled_arr):
             additional_arr = np.concatenate((additional_arr, [nearest_neighbors]), axis=0)
     return np.concatenate((misc_arr, additional_arr), axis=1)
 
+# function that saves images of a people into specific folder with correct label
+# it grabs as input csv file
+# defined in SDD section 3.2.3
 
 def visualization(out_df):
+    # getting current directory
     current_dir = os.getcwd().replace('\\', '/')
     visualization_dir = current_dir + '/data/visualization'
     shutil.rmtree(visualization_dir, ignore_errors = True)
+    # creating visualization folder if it does not exist
+    # otherwise opening visualization folder
     if os.path.isdir(visualization_dir) == False:
         os.makedirs(visualization_dir)
+    # looping through each unique person in csv file
     for person_id in range(out_df['Person ID'].min(), out_df['Person ID'].max()+1):
+        # creating a folder for a person if it does not exist
         person_dir = visualization_dir + '/' + str(person_id)
         if os.path.isdir(person_dir) == False:
             os.makedirs(person_dir)
         person_df = out_df[out_df['Person ID'] == person_id]
+        # looping through each image of a current person and saving it to corresponding folder in visualization folder with correct label
         for index in range(len(person_df['Person ID'].to_list())):
             person_data = person_df.values[index]
             source_file = current_dir + '/data/gallery/' + str(person_data[2]) + '/' + str(person_data[1])
-            # print(source_file)
+            
             filename = str(person_data[3]).replace('/', '_')
             filename = filename.replace(':', '_')
             filename = filename.replace(' ', '_')
             target_file = person_dir + '/' + 'cam_' + str(person_data[2]) + '_time_' + filename + '.jpg'
-            # print(target_file)
+            
             shutil.copyfile(source_file, target_file)
+
+
+
+# Copyright 2021 Missori State University
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
